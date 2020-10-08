b0dab0fca0d80a35ae0894dfc5fbc8d5e4294575
littlelittleany <71093739+littlelittleany@users.noreply.github.com>
2020-10-08 11:53:25 -0700

    Add files via upload

---
+++ character.js
@@ -0,0 +1,41 @@
+class Dong {
+    constructor (family){
+        const {Jo, An, Kit, Yu} = family;
+        this.name = 'Dong';
+        this.type = new dog;
+        this.breed = 'corgi';
+        this.sound = songs.barking;
+        this.friends = [Maffy, 16, Meatball]
+        this.seeking = image.foods ['foods'];
+        this.love = [ball, play];
+    }
+}
+
+eat () {
+    eating(this.seeking, this.love);
+    if (ball > 0) {
+        result = play; 
+    } else if (foods > 0){
+        result = happy;
+    }
+    else {
+        return;
+    }
+}
+
+friends(this.friends) {
+    this.friends = 'soMuchFun';
+}
+
+const x = this.breed;
+
+corgi () {
+    if (x = 'corgi'){
+        return true;
+        play(sound);
+    }
+    else {
+      let newString = 'stop barking';
+  
+    }
+}
